4758297f235594ef4580f0f69dbdfebc067442f0
allison ruiz <allisonruiz@allisons-MacBook-Pro.local>
2022-02-05 12:11:51 -0800

    script.js

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,10 @@
 let quantity = 1;
+
+function updateQuantity(displayQuantity) {
+let quantity= document.querySelector(".total-quantity");
+quantity.textContent = displayQuantity;
+}
+
 const quantDown = document.querySelector("#quantity-down")
 const quantUp = document.querySelector("quantity-up");
 
@@ -6,6 +12,7 @@ const quantUp = document.querySelector("quantity-up");
 quantDown.addEventListener('click', function(e) {
     if (quantity > 0 ) {
     quantity--;
+    updateQuantity(quantity);
 }
 });
 
