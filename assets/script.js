00162231d295b7092e81067f87121b775ec3111b
allison ruiz <allisonruiz@allisons-MacBook-Pro.local>
2022-02-05 13:16:16 -0800

    script.js

--- assets/script.js
+++ assets/script.js
@@ -1,22 +1,28 @@
 let quantity = 1;
+const itemPrice = 15;
+let cartTotal = quantity * itemPrice;
 
 function updateQuantity(displayQuantity) {
 let quantity= document.querySelector(".total-quantity");
 quantity.textContent = displayQuantity;
+dispTotal.textContent = cartTotal;
 }
 
 const quantDown = document.querySelector("#quantity-down")
 const quantUp = document.querySelector("#quantity-up");
+const dispTotal = document.querySelector("h1");
 
 
 quantDown.addEventListener('click', function(e) {
     if (quantity > 0 ) {
     quantity--;
+    cartTotal = quantity * itemPrice;
     updateQuantity(`Quantity: ${quantity}`);
 }
 });
 
 quantUp.addEventListener('click', function (e) {
     quantity++;
+    cartTotal = quantity * itemPrice;
     updateQuantity(`Quantity: ${quantity}`);
 });
